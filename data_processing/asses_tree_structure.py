8e695ff3b31993061476d060e91ebf8d068a595c
halabikeren <halabikeren@gmail.com>
2022-07-28 18:41:16 +0300

    updated analysis to describe coverage during search of monophyletic group

--- data_processing/asses_tree_structure.py
+++ data_processing/asses_tree_structure.py
@@ -7,7 +7,7 @@ import click
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from services.pbs_service import PBSService
-from data_processing.check_tree_monophyly import add_genus_property
+from data_processing.check_tree_monophyly import add_group_by_property
 
 logger = logging.getLogger(__name__)
 
@@ -16,6 +16,18 @@ logger = logging.getLogger(__name__)
 @click.option(
     "--tree_path",
     help="path to tree file in newick format",
+    type=click.Path(exists=True, file_okay=True, readable=True),
+    required=True,
+)
+@click.option(
+    "--group_by",
+    help="taxonomy level to group by",
+    type=click.Choice(["genus", "family"]),
+    required=True,
+)
+@click.option(
+    "--classification_path",
+    help="path to dataframe classifying each leaf to genus/ family",
     type=click.Path(exists=True, file_okay=True, readable=True),
     required=True,
 )
@@ -31,7 +43,24 @@ logger = logging.getLogger(__name__)
     type=click.Path(exists=False),
     required=True,
 )
-def asses_tree(tree_path: str, work_dir: str, log_path: str):
+@click.option(
+    "--output_path",
+    help="path of merged result",
+    type=click.Path(exists=False),
+    required=True,
+)
+@click.option(
+    "--batch_num", help="batches number", type=int, required=False, default=100
+)
+def asses_tree(
+    tree_path: str,
+    group_by: str,
+    classification_path: str,
+    work_dir: str,
+    log_path: str,
+    batch_num: int,
+    output_path: str,
+):
 
     logging.basicConfig(
         level=logging.INFO,
@@ -43,35 +72,55 @@ def asses_tree(tree_path: str, work_dir: str, log_path: str):
     os.makedirs(work_dir, exist_ok=True)
     output_dir = f"{work_dir}/monophyly_status/"
     os.makedirs(output_dir, exist_ok=True)
-    genera_batches_dir = f"{work_dir}/genera/"
+    genera_batches_dir = f"{work_dir}/by_{group_by}/"
     os.makedirs(genera_batches_dir, exist_ok=True)
 
+    classification_data = pd.read_csv(classification_path)
     tree = Tree(tree_path, format=1)
-    add_genus_property(tree)
-    genera = list(set([node.genus for node in tree.get_leaves()]))
+    add_group_by_property(
+        tree=tree, classification_data=classification_data, class_name=group_by,
+    )
+    groups = list(set([leaf.__dict__[group_by] for leaf in tree.get_leaves()]))
+    logger.info(f"analysis of tree with {len(groups)} {group_by} unique values")
 
-    logger.info(f"analysis of tree with {len(genera)} genera")
-    batch_size = int(len(genera)/50)+1
+    batch_size = int(len(groups) / batch_num) + 1
     logger.info(f"determined batch size = {batch_size}")
-    genera_batches = [genera[i:i+batch_size] for i in range(0, len(genera), batch_size)]
+
+    genera_batches = [
+        groups[i : i + batch_size] for i in range(0, len(groups), batch_size)
+    ]
+    logger.info(f"num batches = {len(genera_batches):,}")
+
     jobs_commands = []
     for i in range(len(genera_batches)):
-        genera_path = f"{genera_batches_dir}batch_{i}_genera.csv"
-        pd.Series(genera_batches[i]).to_csv(genera_path, index=False)
+        group_path = f"{genera_batches_dir}batch_{i}_{group_by}.csv"
+        pd.Series(genera_batches[i]).to_csv(group_path, index=False)
         output_path = f"{output_dir}/batch_{i}_monophyly_status.csv"
-        if not os.path.exists(output_path):
-            batch_commands = [f"cd {os.path.dirname(__file__)}", f"python check_tree_monophyly.py --genera_path={genera_path} --tree_path={tree_path} --output_path={output_path}"]
-            jobs_commands.append(batch_commands)
+        batch_commands = [
+            f"cd {os.path.dirname(__file__)}",
+            f"python check_tree_monophyly.py --group_path={group_path} --classification_path={classification_path} --group_by={group_by} --tree_path={tree_path} --output_path={output_path}",
+        ]
+        jobs_commands.append(batch_commands)
     if len(jobs_commands) > 0:
-        logger.info(f"submitting {len(jobs_commands)} jobs for branches of genera")
-        PBSService.execute_job_array(work_dir=f"{work_dir}/jobs/",
-                                     output_dir=f"{work_dir}/jobs_output/",
-                                     jobs_commands=jobs_commands,)
+        logger.info(f"submitting {len(jobs_commands)} jobs for batches of {group_by}")
+        PBSService.execute_job_array(
+            work_dir=f"{work_dir}/jobs/",
+            output_dir=f"{work_dir}/jobs_output/",
+            jobs_commands=jobs_commands,
+        )
 
-    logger.info(f"processing of genera monophyly status is complete, will now merge results")
-    genus_to_monophyly = pd.concat([pd.read_csv(f"{output_dir}{path}") for path in os.listdir(output_dir) if path.endswith(".csv")])
-    genus_to_monophyly.to_csv(tree_path.replace(".tre", "_complete_genus_to_monophyly.csv"), index=False)
+    logger.info(
+        f"processing of genera monophyly status is complete, will now merge results"
+    )
+    group_to_monophyly = pd.concat(
+        [
+            pd.read_csv(f"{output_dir}{path}")
+            for path in os.listdir(output_dir)
+            if path.endswith(".csv")
+        ]
+    )
+    group_to_monophyly.to_csv(output_path, index=False)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asses_tree()
-
--- data_processing/check_tree_monophyly.py
+++ data_processing/check_tree_monophyly.py
@@ -1,4 +1,4 @@
-import re
+import os
 import sys
 from typing import Tuple
 
@@ -10,48 +10,150 @@ import click
 
 logger = logging.getLogger(__name__)
 
-def add_genus_property(tree: Tree):
-    for node in tree.get_leaves():
-        genus = re.split("_|\s", node.name)[0]
-        node.add_feature(pr_name="genus", pr_value=genus)
 
-def get_largest_monophyletic_group(root: Tree, property_value: str, property_name: str = "genus") -> Tuple[Tree, float, bool]:
+def add_group_by_property(
+    tree: Tree, classification_data: pd.DataFrame, class_name: str
+):
+    taxon_to_class = classification_data.set_index("taxon")[class_name].to_dict()
+    for node in tree.get_leaves():
+        node_class = taxon_to_class[node.name]
+        node.add_feature(pr_name=class_name, pr_value=node_class)
+
+
+def get_largest_monophyletic_group(
+    root: Tree, property_value: str, property_name: str, full_group_size: int
+) -> Tuple[
+    Tree, float, bool, pd.DataFrame
+]:  # returns: root of subtree, number of members in the subtree, indicator if the subtree is larger than others, % members covered in the group, % members out of subtree
+    ancestor_to_stats = pd.DataFrame(
+        columns=[
+            "node",
+            "num_members",
+            "size_subtree",
+            "total_members_coverage",
+            "subtree_members_coverage",
+        ]
+    )
+
     if root.is_leaf():
-        return root, 0, False
+        return root, 0, False, ancestor_to_stats
 
     q = [root]
     monophyletic_ancestors_to_size = dict()
     while len(q) > 0:
         node = q.pop(0)
-        property_values = list(set([leaf.__dict__[property_name] for leaf in node.get_leaves()]))
-        if len(property_values) == 1 and property_values[0] == property_value:
+        property_values = [leaf.__dict__[property_name] for leaf in node.get_leaves()]
+        node_stats = {
+            "node": [node.name],
+            "num_members": [property_values.count(property_value)],
+            "size_subtree": [len(node.get_leaves())],
+            "total_members_coverage": [
+                property_values.count(
+                    property_value
+                )  # measure of how many members are covered by the subtree of out of all the members
+                / full_group_size
+            ],
+            "subtree_members_coverage": [
+                (property_values.count(property_value) / len(node.get_leaves()))
+            ],  # measure of how many of the leaves in the subtree are members
+        }
+        ancestor_to_stats = pd.concat(
+            [ancestor_to_stats, pd.DataFrame.from_dict(node_stats, orient="columns")]
+        )
+        if len(set(property_values)) == 1 and property_values[0] == property_value:
             monophyletic_ancestors_to_size[node] = len(node.get_leaves())
+            if node_stats["total_members_coverage"][0] == 1:
+                break
         for child in node.get_children():
             q.append(child)
-    best_node = max(monophyletic_ancestors_to_size, key=monophyletic_ancestors_to_size.get)
-    is_larger_than_rest = len([n for n in monophyletic_ancestors_to_size if monophyletic_ancestors_to_size[n] == monophyletic_ancestors_to_size[best_node]]) == 1
-    return best_node, monophyletic_ancestors_to_size[best_node], is_larger_than_rest
+    best_node = max(
+        monophyletic_ancestors_to_size, key=monophyletic_ancestors_to_size.get
+    )
+    is_larger_than_rest = (
+        len(
+            [
+                n
+                for n in monophyletic_ancestors_to_size
+                if monophyletic_ancestors_to_size[n]
+                == monophyletic_ancestors_to_size[best_node]
+            ]
+        )
+        == 1
+    )
+    return (
+        best_node,
+        monophyletic_ancestors_to_size[best_node],
+        is_larger_than_rest,
+        ancestor_to_stats,
+    )
 
 
-def check_monophyly(record: pd.Series, tree: Tree, property_name: str = "genus") -> Tuple[bool, str, str, str, float, float]:
-    genus = record.genus
-    genus_leaves = [leaf for leaf in tree.get_leaves() if leaf.genus == genus]
-    if len(genus_leaves) < 2:
-        return True, "monophyletic", "", "".join([l.name for l in genus_leaves]), 1, 0
-    is_monophyletic, clade_type, monophyly_violators = tree.check_monophyly(values=[genus], target_attr=property_name)
-    monophyly_violators_names = ",".join([node.name for node in monophyly_violators if node.is_leaf()]) if len(monophyly_violators) > 0 else np.nan
-    lca = tree.get_common_ancestor(genus_leaves)
-    largest_monophyly_root, largest_monophyly_size, is_larger_than_rest = get_largest_monophyletic_group(root=lca, property_value=genus, property_name="genus")
+def check_monophyly(
+    record: pd.Series, tree: Tree, property_name: str, ancestor_to_stats_path: str
+) -> Tuple[bool, str, str, str, float, float]:
+    group = record[property_name]
+    group_leaves = [
+        leaf for leaf in tree.get_leaves() if leaf.__dict__[property_name] == group
+    ]
+    if len(group_leaves) < 2:
+        return (
+            True,
+            "monophyletic",
+            "",
+            "".join([l.name for l in group_leaves]),
+            1,
+            0,
+        )
+    is_monophyletic, clade_type, monophyly_violators = tree.check_monophyly(
+        values=[group], target_attr=property_name
+    )
+    monophyly_violators_names = (
+        ",".join([node.name for node in monophyly_violators if node.is_leaf()])
+        if len(monophyly_violators) > 0
+        else np.nan
+    )
+    lca = tree.get_common_ancestor(group_leaves)
+    (
+        largest_monophyly_root,
+        largest_monophyly_size,
+        is_larger_than_rest,
+        ancestor_to_stats,
+    ) = get_largest_monophyletic_group(
+        root=lca,
+        property_value=group,
+        property_name=property_name,
+        full_group_size=len(group_leaves),
+    )
     largest_monophyletic_group_names = largest_monophyly_root.get_leaf_names()
-    fraction_largest_monophyletic_group = float(largest_monophyly_size)/float(len(genus_leaves))
-    fraction_of_invaders = len(monophyly_violators) / (len(genus_leaves)+len(monophyly_violators))
-    return bool(is_monophyletic), str(clade_type), monophyly_violators_names, largest_monophyletic_group_names, fraction_largest_monophyletic_group, fraction_of_invaders
+    fraction_largest_monophyletic_group = float(largest_monophyly_size) / float(
+        len(group_leaves)
+    )
+    fraction_of_invaders = len(monophyly_violators) / (
+        len(group_leaves) + len(monophyly_violators)
+    )
+    ancestor_to_stats.to_csv(ancestor_to_stats_path, index=False)
+
+    return (
+        bool(is_monophyletic),
+        str(clade_type),
+        monophyly_violators_names,
+        largest_monophyletic_group_names,
+        fraction_largest_monophyletic_group,
+        fraction_of_invaders,
+    )
+
 
 @click.command()
 @click.option(
-    "--genera_path",
-    help="path csv with the genera to compute monophyly for",
+    "--group_path",
+    help="path csv with the groups values to compute monophyly for",
     type=click.Path(exists=True, file_okay=True, readable=True),
+    required=True,
+)
+@click.option(
+    "--group_by",
+    help="item to group by (either genus of family)",
+    type=click.Choice(["genus", "family"]),
     required=True,
 )
 @click.option(
@@ -61,12 +163,24 @@ def check_monophyly(record: pd.Series, tree: Tree, property_name: str = "genus")
     required=True,
 )
 @click.option(
+    "--classification_path",
+    help="path with mapping of each taxon to genus/ family",
+    type=click.Path(exists=True, file_okay=True, readable=True),
+    required=True,
+)
+@click.option(
     "--output_path",
     help="path to write output to",
     type=click.Path(exists=False),
     required=True,
 )
-def compute_genera_monophyly_classification(genera_path: str, tree_path: str, output_path: str):
+def compute_genera_monophyly_classification(
+    group_path: str,
+    group_by: str,
+    classification_path: str,
+    tree_path: str,
+    output_path: str,
+):
 
     logging.basicConfig(
         level=logging.INFO,
@@ -76,16 +190,40 @@ def compute_genera_monophyly_classification(genera_path: str, tree_path: str, ou
     )
 
     tree = Tree(tree_path, format=1)
-    add_genus_property(tree)
-    logger.info(f"parsed input tree and genera classification per leaf")
+    classification_data = pd.read_csv(classification_path)
+    add_group_by_property(
+        tree, classification_data=classification_data, class_name=group_by
+    )
+    logger.info(f"parsed input tree and groups classification per leaf")
 
-    genera = pd.read_csv(genera_path)["0"].tolist()
-    logger.info(f"checking monophyly status for {len(genera)} genera")
+    groups = pd.read_csv(group_path)["0"].tolist()
+    logger.info(f"checking monophyly status for {len(groups)} groups")
 
-    genus_to_monophyly_status = pd.DataFrame(columns=["genus", "is_monophyletic", "clade_type", "monophyly_violators"])
-    genus_to_monophyly_status["genus"] = genera
-    genus_to_monophyly_status[["is_monophyletic", "clade_type", "monophyly_violators", "largest_monophyly_members", "largest_monophyletic_fraction", "fraction_of_invaders"]] = genus_to_monophyly_status.apply(lambda record: check_monophyly(record=record, tree=tree), axis=1, result_type="expand")
-    genus_to_monophyly_status.to_csv(output_path, index=False)
+    group_to_monophyly_status = pd.DataFrame(
+        columns=[group_by, "is_monophyletic", "clade_type", "monophyly_violators"]
+    )
+    group_to_monophyly_status[group_by] = groups
+    group_to_monophyly_status[
+        [
+            "is_monophyletic",
+            "clade_type",
+            "monophyly_violators",
+            "largest_monophyly_members",
+            "largest_monophyletic_fraction",
+            "fraction_of_invaders",
+        ]
+    ] = group_to_monophyly_status.apply(
+        lambda record: check_monophyly(
+            record=record,
+            tree=tree,
+            property_name=group_by,
+            ancestor_to_stats_path=f"{os.path.dirname(output_path)}/{group_by}_{record[group_by]}_ancestry_stats.csv",
+        ),
+        axis=1,
+        result_type="expand",
+    )
+    group_to_monophyly_status.to_csv(output_path, index=False)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     compute_genera_monophyly_classification()
