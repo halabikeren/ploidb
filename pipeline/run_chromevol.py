8b0ffe3979b64a358612bfd0062442e0fad516b8
halabikeren <halabikeren@gmail.com>
2022-11-20 18:27:51 +0200

    fixed documentation error

--- pipeline/run_chromevol.py
+++ pipeline/run_chromevol.py
@@ -2,13 +2,15 @@ import json
 import click
 import sys
 import os
+
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from data_generation.chromevol import ChromevolExecutor
+
 
 @click.command()
 @click.option(
     "--input_path",
-    help="csv with query names to apply name resolution on",
+    help="json with the input for chromevol execution",
     type=click.Path(exists=True, file_okay=True, readable=True),
     required=True,
 )
@@ -17,6 +19,6 @@ def run_chromevol(input_path: str):
         input_args = json.load(fp=infile)
     ChromevolExecutor.run(input_args=input_args)
 
-if __name__ == '__main__':
-    run_chromevol()
 
+if __name__ == "__main__":
+    run_chromevol()
