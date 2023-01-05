be7f3833f6d3c866df1e20a519e659f73a2fd56b
halabikeren <halabikeren@gmail.com>
2023-01-05 09:33:00 +0200

    added option to use model weighting in simulations

--- simulate.py
+++ simulate.py
@@ -83,6 +83,13 @@ logger = logging.getLogger(__name__)
     required=False,
     default=1000,
 )
+@click.option(
+    "--use_model_selection",
+    help="indicator of weather model selection should be used",
+    type=bool,
+    required=False,
+    default=True,
+)
 def simulate(
     counts_path: str,
     tree_path: str,
@@ -95,6 +102,7 @@ def simulate(
     max_parallel_jobs: int,
     simulations_num: int,
     trials_num: int,
+    use_model_selection: bool,
 ):
 
     logging.basicConfig(
@@ -110,36 +118,42 @@ def simulate(
     start_time = timer()
 
     os.makedirs(work_dir, exist_ok=True)
-    pipeline = Pipeline(work_dir=work_dir,
-                        parallel=parallel,
-                        ram_per_job=ram_per_job,
-                        queue=queue,
-                        max_parallel_jobs=max_parallel_jobs)
-
+    pipeline = Pipeline(
+        work_dir=work_dir, parallel=parallel, ram_per_job=ram_per_job, queue=queue, max_parallel_jobs=max_parallel_jobs
+    )
 
     logger.info(f"selecting the best chromevol model")
-    best_model_results_path = pipeline.get_model_weights(
+    model_selection_dir = f"{work_dir}/model_selection/"
+    if os.path.exists(model_selection_dir):
+        for model_name in os.listdir(model_selection_dir):
+            out_path = f"{model_selection_dir}{model_name}/parsed_output.json"
+            if os.path.exists(out_path):
+                os.remove(out_path)
+
+    model_to_weight = pipeline.get_model_weights(
         counts_path=counts_path,
         tree_path=tree_path,
+        use_model_selection=use_model_selection,
     )
+    best_model_results_path = list(model_to_weight.keys())[0]
 
     os.makedirs(simulations_dir, exist_ok=True)
     logger.info(f"simulating data based on selected model = {best_model_results_path}")
     simulations_dirs = pipeline.get_simulations(
-                simulations_dir=simulations_dir,
-                orig_counts_path=counts_path,
-                tree_path=tree_path,
-                model_parameters_path=best_model_results_path,
-                simulations_num=simulations_num,
-                trials_num=trials_num,
-            )
+        simulations_dir=simulations_dir,
+        orig_counts_path=counts_path,
+        tree_path=tree_path,
+        model_parameters_path=best_model_results_path,
+        simulations_num=simulations_num,
+        trials_num=trials_num,
+    )
 
-    print(f"simulations were generated in {os.path.dirname(simulations_dirs[0])}")
+    print(f"simulations were generated in {simulations_dir}")
 
     end_time = timer()
 
     logger.info(f"duration = {timedelta(seconds=end_time-start_time)}")
 
-if __name__ == '__main__':
-    simulate()
 
+if __name__ == "__main__":
+    simulate()
